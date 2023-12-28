a1a506e3d63a73f988c30c7b53eceb5a285a668e
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2023-12-28 10:43:09 +0000

    memory config done

---
+++ Basics/Memory.js
@@ -0,0 +1,22 @@
+// Stack(Primitive), Heap Memory(NP)
+
+let myYoutubeName = "Coder"
+
+let anotherName = myYoutubeName
+anotherName = "Coding"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user1@ybl"
+}
+
+
+let userTwo = userOne
+
+userTwo.email = "user2@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
