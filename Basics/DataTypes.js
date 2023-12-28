f36c93a13d7e44c6dbe39445259686cc59261e47
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2023-12-28 09:24:05 +0000

    container config done

---
+++ Basics/DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // This lines ensures that all code is treated in accordance to new version of JS
+
+//alert(1+9)  // This is wrong syntax in Node,it can run in Browser
+
+console.log(3+3); 
+
+console.log("hi")
+
+let name = "Rahul"
+let age = 12
+let isLoggedIn = false
+
+// Data types
+
+// number -->(2)^53
+// bigint -->
+// string -->""/''
+// boolean --> true/false
+// null --> standalone value empty value
+// undefined --> not assigned value
+// symbol --> uniqueness
+
+// object
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
