955930ac2a6276d705a14e94807295f8fb41a039
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2023-12-28 09:52:24 +0000

    conversion of data types

---
+++ Basics/CAndO.js
@@ -0,0 +1,31 @@
+//let score = '33abc'
+//let score = null
+//let score = undefined
+//let score = true
+//let score = "hi"
+
+// "22" -- 22
+// "22jbj" -- 22jbj
+// true -- 1, false -- 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 -- true, 0 -- false 
+// "" -- false 
+// "hi" -- true 
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
