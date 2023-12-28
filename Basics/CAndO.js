0329d85187157ce65a8b450c8597f49fa31e6770
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2023-12-28 10:05:29 +0000

    Conversion config done

--- Basics/CAndO.js
+++ Basics/CAndO.js
@@ -1,3 +1,4 @@
+// ************* Conversion *******************
 //let score = '33abc'
 //let score = null
 //let score = undefined
@@ -25,7 +26,50 @@
 // console.log(valueInNumber);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************* Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " Rahul"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+ "2");
+// console.log("1"+ 2 +2);
+// console.log(1+2+"2");
+
+// console.log(+true); //1
+// //console.log(true+);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
