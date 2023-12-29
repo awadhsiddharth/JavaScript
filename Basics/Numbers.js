482cdcc9e1257ad311df36522af671a0fcf7db5f
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2023-12-29 05:42:08 +0000

    numbers config done

---
+++ Basics/Numbers.js
@@ -0,0 +1,40 @@
+// // Numbers
+
+// const score = 550;
+// console.log(score);
+
+
+// const balance = new Number(666);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+// const otherNum = 1123.9998;
+
+// console.log(otherNum.toPrecision(3))
+
+// const nums = 1000000
+// console.log(nums.toLocaleString('en-IN'));
+
+// Maths
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(3,4,9,10));
+// console.log(Math.max(3,4,9,10));
+
+// Math.random method gives value b/t 0 and 1
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()  * (max - min + 1)) + min);
