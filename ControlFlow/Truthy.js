4696bfe052239bf31eb8e66c4bf8e96290adec12
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-02 09:43:38 +0000

    truthy config done

---
+++ ControlFlow/Truthy.js
@@ -0,0 +1,41 @@
+// const userEmail = []
+
+// // if (userEmail) {
+// //     console.log("Got user email");
+// // }
+// // else{
+// //     console.log("Don't have user email");
+// // }
+
+// /*
+// Falsy Values--
+// false,0,-0, BigInt 0n,"", null, undefined, NaN
+
+// Truthy Values--
+// "0","false"," ",[],{},function(){}
+// */
+
+// // if (userEmail.length ===0) {
+// //     console.log("Array is empty");
+// // }
+
+// // const emptyObj = {}
+// // if(Object.keys(emptyObj).length===0){
+// //     console.log("Object is empty");
+// // }
+
+// // Nullish Coalescing operator (??): null undefined
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// // val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// // Terniary operator
+// // condition > true : false
+
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
