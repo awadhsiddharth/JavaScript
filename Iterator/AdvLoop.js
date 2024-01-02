88920a71c834e642b9b892be17f7b250f336ea48
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-02 11:25:22 +0000

    advl config done

---
+++ Iterator/AdvLoop.js
@@ -0,0 +1,37 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+// const greeting = "Hello World"
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps- unique value and preserve order
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('RF',"Russian Federation")
+map.set('IN',"India")
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,':-', value);
+// }
+
+// const myObject={
+//     game1 : "NFS",
+//     game2 : "Spideman"
+// }
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ":-",value);
+// }
