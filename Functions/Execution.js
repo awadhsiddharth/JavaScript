6c38ac8b6aa1b9a4d8f1c2dce6f022168f27d718
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-02 07:21:06 +0000

    execution config done

---
+++ Functions/Execution.js
@@ -0,0 +1,12 @@
+/* Javascript execution context - This means how js runs our file
+   JS runs our file in two phase:
+   1. Global Execution Context(this) 
+   Browser GEC - Window
+   Nodejs GEC - this
+   other environment GEC - 
+   2. Function Execution Context
+   3. Eval Execution Context
+
+   // 1.Memory creation phase(1)- memory allocation
+   // 2.Exection Phase - runs business logic
+   */
