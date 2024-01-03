c91ebffa6c761907b64e37553555cef31a9a7b7c
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-03 04:47:26 +0000

    adv config done

---
+++ Iterator/AdvLoops.js
@@ -0,0 +1,17 @@
+// const myObject = {
+//     js : "javascript",
+//     cpp : "C++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     // console.log(myObject[key]);
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js","rb","py","java","cpp"]
+for (const key in object) {
+    console.log(key);
+    console.log(programming[key]);
+}
