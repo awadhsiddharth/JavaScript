38b8dd1592b5114f515f737825d1728d113395ec
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-09 06:58:14 +0000

    fetch done

---
+++ Promises/fetch.js
@@ -0,0 +1,7 @@
+// The Fetch API provides a JavaScript
+// interface for accessing and manipulating
+// parts of the protocol, such as requests
+// and responses. It also provides a global
+// fetch() method that provides an easy,
+// logical way to fetch resources asynchronously
+// across the network.
