73ba1b8623655688db39e0c8dc09dd630beea0eb
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-15 07:14:23 +0000

    a7 done

---
+++ Basics/a7.js
@@ -0,0 +1,9 @@
+console.log("Difference among var , let and const")
+var a =  56;
+var v = "Vivek";
+var c = null;
+var d = undefined;
+var e = true;
+//avoid using var keyword and use let
+const author = "Vivek";
+// let author = 5; // throws an error because constant cant be changed
