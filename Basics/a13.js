5736621fe532ea6fa313031bf27292496a3e2444
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 04:51:10 +0000

    a13 done

---
+++ Basics/a13.js
@@ -0,0 +1,16 @@
+// let name = "Harry";
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(1, 5));
+// console.log(name.slice(1));
+// console.log(name.replace("Har", "Per"));
+// let friend = "Naman";
+// console.log(name.concat(" is a friend of ", friend));
+// let friend1 = "   Meena   ";
+// console.log(friend1);
+// console.log(friend1.trim());
+
+// let fr = "Shivika" + "Harry";
+// console.log(fr[1])
+// console.log(fr)
