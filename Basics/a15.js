50a271233d833270df8d94f999b647c5d8c85088
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 04:52:41 +0000

    a15 done

---
+++ Basics/a15.js
@@ -0,0 +1,24 @@
+let num = [3, 54, 1, 2, 4];
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+// for each loop
+// num.forEach((element) => {
+//   console.log(element * element);
+// });
+
+// Array.from
+let name = "harry";
+let arr = Array.from(name);
+console.log(arr);
+
+// for .of loop
+for (let i of num) {
+  console.log(i);
+}
+
+//for ..in loop
+for (let item in num) {
+  console.log(num[item]);
+  console.log(item);
+}
