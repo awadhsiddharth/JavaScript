3fc46e0a75c051cac8a73a30773e1efe90d003c5
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 04:50:32 +0000

    a12 done

---
+++ Basics/a12.js
@@ -0,0 +1,24 @@
+// //String
+// // let name = "Rahul";
+// // console.log(name.length);
+// // console.log(name[0]);
+// // console.log(name[1]);
+// // console.log(name[2]);
+// // console.log(name[3]);
+// // console.log(name[4]);
+// // let friend = "Vivek";
+// // console.log(friend.length);
+
+// // Template literals
+// let boy1 = "Arjun";
+// let boy2 = "Nikhil";
+// // Nikhil is a friend of Arjun
+// let sentence = `${boy2} is a friend of ${boy1}`;
+// console.log(sentence);
+
+// escape sequence characters(\)
+let fruit = "Bana\rna";
+let fruit1 = "Bana\tna";
+console.log(fruit);
+console.log(fruit1);
+console.log(fruit.length);
