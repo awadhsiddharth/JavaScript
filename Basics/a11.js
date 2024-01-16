a4e5782294b3b43c5ac5abb1d156ec58fca6dd82
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 04:49:38 +0000

    a11 done

---
+++ Basics/a11.js
@@ -0,0 +1,34 @@
+// // let marks = {
+// //   harry: 99,
+// //   shubham: 90,
+// //   rahul: 78,
+// //   jayesh: 66,
+// // };
+// // // for (let i = 0; i < Object.keys(marks).length; i++) {
+// // //   console.log(
+// // //     "The marks of " +
+// // //       Object.keys(marks)[i] +
+// // //       " are: " +
+// // //       marks[Object.keys(marks)[i]],
+// // //   );
+// // // }
+
+// // for(let key in marks){
+// //   console.log(key)
+// //   console.log("The marks of " + key + " are: " +marks[key]);
+// // }
+
+// let cn = 4;
+// let i;
+// while (i != cn) {
+//   console.log("Try Again!")
+//   i = prompt("Enter a  number: ");
+// }
+// console.log("You have a entered a correct number");
+
+const mean = (a, b, c, d) => {
+    return (a + b + c + d) / 4;
+  };
+  let aa = mean(2, 3, 4, 5);
+  console.log(aa);
+  
