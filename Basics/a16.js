d3c9c7b3b7b98e621b78abe833fe220fb0131d8d
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 04:53:46 +0000

    a16 done

---
+++ Basics/a16.js
@@ -0,0 +1,26 @@
+let arr = [45, 23, 21];
+// Array map method
+let a = arr.map((value, index, array) => {
+  //console.log(value, index, array);
+  return value;
+});
+//console.log(arr);
+//console.log(a);
+
+// Array filter method
+let arr1 = [45, 23, 21, 0, 3, 5];
+let a1 = arr1.filter((value1) => {
+  return value1 < 5;
+});
+//console.log(a1,arr1);
+
+// Array reduce method
+let arr2 = [1, 2, 3, 5, 2, 1];
+function reduce_function(h1, h2) {
+  return h1 + h2;
+}
+let ar = arr2.reduce(reduce_function);
+// let ar = arr2.reduce((h1, h2) => {
+//   return h1 + h2;
+// });
+console.log(ar);
