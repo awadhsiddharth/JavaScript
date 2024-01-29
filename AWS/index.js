c4b1d31adf56663e073384a884677c0843a973fa
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-29 10:57:18 +0530

    Add files via upload

---
+++ AWS/index.js
@@ -0,0 +1,28 @@
+exports.handler = async(event)=>{
+
+    var response = {
+        statusCode : 404,
+        body : JSON.stringify({
+            Error : "No routes",
+        }),
+    };
+    if(event.path="/api/getproduct"){
+        response = {
+            statusCode : 200,
+            body : JSON.stringify({
+                "ProductName" : "shoes",
+                "Price" : "100$"
+            }),
+        };
+    }
+    if(event.path= "api/getuser"){
+        response = {
+            statusCode : 200,
+            body : JSON.stringify({
+                "UserName" : "Rahul Jacob",
+                "Email" : "rahulgjacob@gmail.com"
+            }),
+        };
+    }
+    return response
+};
