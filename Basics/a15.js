befeaaf0ae4c3ca2e4b241173874e7112570eaed
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-05-30 09:06:53 +0530

    Update a15.js

--- Basics/a15.js
+++ Basics/a15.js
@@ -1,7 +1,7 @@
 let num = [3, 54, 1, 2, 4];
 // for (let i = 0; i < num.length; i++) {
 //   console.log(num[i]);
-// }
+// } 
 // for each loop
 // num.forEach((element) => {
 //   console.log(element * element);
